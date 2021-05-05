70590dfcd4c58066ca8d097ce3def3057e7a5d9d
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-05-05 12:23:04 -0300

    Passed all tests

--- src/index.js
+++ src/index.js
@@ -44,19 +44,27 @@ function checksTodoExists(request, response, next) {
     return response.status(400).json({ error: "Not a valid ID" });
   }
 
-  const todo = users.todos.find(todo => todo.id === id);
+  const todo = user.todos.find(todo => todo.id === id);
   if (!todo) {
     return response.status(404).json({ error: "TODO not found" });
   }
 
-  response.user = user;
-  response.todo = todo;
+  request.user = user;
+  request.todo = todo;
 
   return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return response.status(404).json({ error: "User ID not found" });
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
